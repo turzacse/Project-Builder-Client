61f44922ca28436ad262dce3e4660306fca137a7
turzacse <turzacse@gmail.com>
2023-12-30 00:47:00 +0600

    dashboar project setup as well as route

--- src/Pages/Projects/Project.jsx
+++ src/Pages/Projects/Project.jsx
@@ -4,9 +4,10 @@ const Project = () => {
     return (
         <div className='my-5 mx-20'>
             <h2 className='text-black text-4xl font-normal'>My Projects</h2>
-            
+
         </div>
     );
 };
+
 
 export default Project;
