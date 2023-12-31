df2cc8cce63b0743dc4271d487b9d5cd472b016f
turzacse <turzacse@gmail.com>
2023-12-31 15:01:10 +0600

    implement the delete functionality

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -4,6 +4,7 @@ import { CiSquareMore } from "react-icons/ci";
 import { MdMore } from "react-icons/md";
 import { MdBrowserUpdated } from "react-icons/md";
 import { MdAutoDelete } from "react-icons/md";
+import Swal from "sweetalert2";
 
 
 
@@ -19,6 +20,39 @@ const Card = () => {
             })
     }, [])
 
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              
+             fetch(`http://localhost:5001/project/${_id}`,{
+                method: 'DELETE'
+             })
+             .then(res => res.json())
+             .then(data => {
+                if(data.deletedCount > 0) {
+                    Swal.fire(
+                        'Deleted!',
+                        'Your item has been deleted.',
+                        'success'
+                      )
+                      const remain = project.filter(cart => cart._id !== _id);
+                      console.log(remain);
+                      setProject(remain);
+                }
+             })
+            }
+          })
+    }
+
     console.log(project);
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
@@ -33,7 +67,9 @@ const Card = () => {
                             <div className="card-actions justify-evenly">
                                 <Link  to={`/details/${item._id}`} className="btn bg-yellow-200 text-2xl text-green-500"><MdMore/></Link>
                                 <Link  to={`/details/${item._id}`} className="btn bg-yellow-200 text-2xl"><MdBrowserUpdated/></Link>
-                                <Link className="btn bg-yellow-200 text-2xl text-red-500"><MdAutoDelete/></Link>
+                                <button
+                                onClick={() => handleDelete(item._id)}
+                                className="btn bg-yellow-200 text-2xl text-red-500"><MdAutoDelete/></button>
                             </div>
                         </div>
                     </div>
