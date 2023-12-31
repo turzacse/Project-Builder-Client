2b2e85157ec447979b5150673032cc54a6e89e4d
turzacse <turzacse@gmail.com>
2023-12-31 16:54:14 +0600

    firebase setup

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDk1fdyeEVEOQavqp3kQk9-VU4cBCtiPHw",
+  authDomain: "necleo-project-builder.firebaseapp.com",
+  projectId: "necleo-project-builder",
+  storageBucket: "necleo-project-builder.appspot.com",
+  messagingSenderId: "704817596896",
+  appId: "1:704817596896:web:964401178a5f26d5728385"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
